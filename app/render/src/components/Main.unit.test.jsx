6e9bacaa15a064ddfaded7807bbe0880e37f295a
Quinn <quinn@Michaels-MacBook-Pro.local>
2021-11-18 18:50:34 -0800

    first working test case

--- app/render/src/components/Main.unit.test.jsx
+++ app/render/src/components/Main.unit.test.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Main from './Main';
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 describe('Main component elements exist', () => {
-  render(<Main />);
-  const container = screen.getByTestId('container');
-  expect(container).toBeInTheDocument;
+  test('Renders a main \'container\' div', () => {
+    render(
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Main />}/>
+        </Routes>
+      </HashRouter>
+    );
+    const container = screen.getByTestId('container');
+    expect(container).toBeInTheDocument;
+  });
 });
