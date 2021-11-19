17b784de7fef1e23bf22cbf5b3c7c4e2363d39a7
Quinn <quinn@Michaels-MacBook-Pro.local>
2021-11-18 16:43:51 -0800

    refactoring to focus primarily on use of React Testing Library

--- app/render/src/components/Main.jsx
+++ app/render/src/components/Main.jsx
@@ -10,10 +10,9 @@ const Main = (props) =>{
 		
 	};
 
-
 	return (
 		<>
-			<div className="container">
+			<div className="container" testId="container">
 				<div id='uploader' className='app__uploader'>
 					<div className='uploader__button-area'>
 							<button className='uploader__button' id='uploader-button'>Click To Add Folders</button>
@@ -32,6 +31,5 @@ const Main = (props) =>{
 		</>
 	);
 };
-
 
 export default Main;
