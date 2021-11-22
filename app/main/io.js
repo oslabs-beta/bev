7b49792b29b188939f77a9dca12cc5c5c959914d
savoy1211 <ryanlee250@gmail.com>
2021-11-22 10:24:07 -0800

    Change location of bundle stats directory

--- app/main/io.js
+++ app/main/io.js
@@ -13,7 +13,8 @@ const exec = util.promisify(require('child_process').exec);
 const notification = require( './notification' );
 
 // Get application directory
-const appDir = path.resolve( os.homedir(), 'BEV-project-files' );
+// const appDir = path.resolve( os.homedir(), 'BEV-project-files' );
+const appDir = path.resolve('./BEV-project-files' );
 // const appDir = path.resolve('/BEV-project-files' );
 
 // Folder json name
@@ -89,12 +90,12 @@ exports.watchFiles = ( win ) => {
 exports.generateDependencyObject = (folderArr) =>{
 	const ARRAY_OF_FILES_AND_DIRS_TO_CRUISE = folderArr;
 	const cruiseOptions = {
-		includeOnly: ["src", "assets", "node_modules"],
+		// includeOnly: ["src", "assets", "node_modules"],
 		exclude: {
 			// path: ["release", "public", "dist"]
 		},
 		doNotFollow: {
-			"path": "node_modules",
+			path: "node_modules",
 		},
 		reporterOptions: {
 			dot: {
@@ -134,12 +135,12 @@ exports.generateBundleInfoObject = async (folders) =>{
 	console.log('folders in io.js', folders)
 	let i = 0;	
 	let fileName;
-
+	const dateTag = new Date().toISOString().slice(0,19).replace('T','').replaceAll(':', '-').replaceAll('-','');
 	const outputBundleObjectsArray = [];
-
+	const outputBundleObjectsArrayRAW = [];
 	for(let folder of folders){
 		fileName = folder.replaceAll(':','');
-		fileName = (fileName.split('').includes('\\')) ? `stats-${fileName.replaceAll('\\','-')}` : `stats-${fileName.replaceAll('/','-')}`;
+		fileName = (fileName.split('').includes('\\')) ? `stats-${fileName.replaceAll('\\','-')}-${dateTag}` : `stats-${fileName.replaceAll('/','-')}-${dateTag}`;
 		console.log("stats-folder.replaceAll('\\','-') :", fileName.replaceAll('\\','-'));
 		console.log('fileName ', fileName);
 		const filepath = path.resolve(appDir, fileName);
@@ -147,7 +148,7 @@ exports.generateBundleInfoObject = async (folders) =>{
 
 		//If stats file does not exist then create
 		if(!fs.existsSync( `${filepath}.json` ) ){
-			const {stdout, stderr} = await exec(`webpack --profile --json > ${appDir}/${fileName}.json`,{cwd: folder});
+			const {stdout, stderr} = await exec(`webpack --profile --json > ${filepath}.json`,{cwd: folder});
 
 			if (stderr) {
 				console.log('stderr', stderr);
@@ -159,8 +160,9 @@ exports.generateBundleInfoObject = async (folders) =>{
 
 		//Read from stats file and store in outputBundleObjectsArray
 		const outputObj = {};
-		const rawStats = fs.readFileSync(`${appDir}/${fileName}.json`);
+		const rawStats = fs.readFileSync(`${filepath}.json`);
 		const stats = JSON.parse(rawStats);
+		outputBundleObjectsArrayRAW.push(stats);
 		const {assets, modules} = stats;
 		let totalSize = 0;
 
@@ -208,6 +210,52 @@ exports.generateBundleInfoObject = async (folders) =>{
 	};
 	console.log('folders', folders);
 
-	return outputBundleObjectsArray;
+	return {bundleStatsRaw: outputBundleObjectsArrayRAW, bundleStats: outputBundleObjectsArray};
 }	
 
+/*
+  `depCruiserResults` is an Object
+  `statsResults` is an Array of Objects
+*/
+exports.modifyDependencyObject = (depCruiserResults, statsResults) =>{
+
+
+  // Traverse `dependencies` array in `depCruiserResults.modules`
+  depCruiserResults.modules.map(m => {
+    // Add `dependencies[n].resolved` to `targetNodeNames`
+    m.dependencies.map(d => {
+      const source = m.source;
+      const target = d.resolved;
+      for (let i = 0; i < statsResults.length; i += 1) {
+
+        const statsArrayTargetInfo = statsResults[i].modules.filter(module => module.name.slice(2) === target)
+        if (statsArrayTargetInfo.length > 0) {
+          statsArrayTargetInfo[0].reasons.forEach(r => {
+            const statsArraySource = r.resolvedModule.slice(2);
+            if (!d.hasOwnProperty('active')) {
+              if (statsArraySource === source && r.type === 'harmony import specifier') {
+                const {active} = r;
+                d.active = active ?? false;
+              }
+            } 
+          })
+        }
+      }
+
+      return d
+    })
+
+    return m;
+  })
+
+  // targetNodeNames = [...new Set(targetNodeNames)] // Remove duplicates
+
+  // To find whether target dependency is used by source: is `active` true or false? 
+  // targetNodeNames.forEach(node => {
+  //   // Find module Object in `statsResults`
+
+  // })
+	
+	// Return mutated depCruiserResults Object
+	return depCruiserResults;
+}
